82e5309ce0067a4a4d484f18dd787b5d03db3085
Sid <sid.aims93@gmail.com>
2023-11-09 11:50:41 +0530

    Fix

--- resources/views/theme_popups.blade.php
+++ resources/views/theme_popups.blade.php
@@ -48,7 +48,7 @@
 
 <!-- Bootstrap Modal -->
 <a class="button" id="popupModal" style="visibility:hidden" href="#popup1">Let me Pop up</a>
-<div class="modal fade" id="popup1" tabindex="-1" aria-labelledby="desiModalLabel" aria-hidden="true">
+<div class="modal" id="popup1" tabindex="-1" aria-labelledby="desiModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
           <div class="modal-header">
