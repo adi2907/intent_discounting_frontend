12c3ad9741ce8f9e7870b9c5c454ef2e03fa78ed
adi2907 <adityaganguli@gmail.com>
2023-11-15 09:40:11 +0530

    fix

--- resources/views/theme_popups.blade.php
+++ resources/views/theme_popups.blade.php
@@ -191,8 +191,7 @@
     </div>
 </div>
 -->
-<!DOCTYPE html>
-<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -288,4 +287,4 @@
 
 
 </body>
-</html>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
