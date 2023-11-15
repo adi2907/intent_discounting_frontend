da9305c9be052047ad851b81fa7010bc06bbc188
adi2907 <adityaganguli@gmail.com>
2023-11-15 09:38:34 +0530

    fix

--- resources/views/theme_popups.blade.php
+++ resources/views/theme_popups.blade.php
@@ -136,7 +136,7 @@
     font-family: 'Montserrat', sans-serif;
   }
 </style>
--->
+
 <a class="button" id="popupModal" style="visibility:hidden" href="#popup1">Let me Pop up</a>
 <div style="font-family: Arial, sans-serif; height: 100vh;" id="popup1" class="overlay">
     <div class="modal" id="popup1" tabindex="-1" aria-labelledby="desiModalLabel" aria-hidden="true">
@@ -144,7 +144,7 @@
             <div class="modal-content">
             <div class="modal-header">
                 <div class="header-box mx-auto text-center">
-                    <!-- <img src="images/brand_logo.png" alt="Brand Logo" class="brand-logo"> -->
+                    <img src="images/brand_logo.png" alt="Brand Logo" class="brand-logo"> 
                     <h5 class="modal-title">Become an Insider</h5>
                 </div>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -169,7 +169,7 @@
         </div>
     </div>
 </div>
-<!-- Bootstrap Modal
+ Bootstrap Modal
 <a class="button" id="popupModal" style="visibility:hidden" href="#popup1">Let me Pop up</a>
 <div style="font-family: Arial, sans-serif; height: 100vh;" id="popup1" class="overlay">
     <div class="popup">
@@ -191,3 +191,101 @@
     </div>
 </div>
 -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Popup Modal</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        /* Overlay styling */
+        .overlay {
+            display: none; /* Hide until triggered */
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: rgba(0, 0, 0, 0.5); /* Dimmed background */
+            z-index: 1050; /* Ensures overlay is above other content */
+            justify-content: center;
+            align-items: center;
+        }
+
+        /* Modal styling */
+        .modal-custom {
+            background: white;
+            border-radius: 5px;
+            box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+            overflow: hidden; /* Ensures the content does not overflow the modal's border-radius */
+        }
+
+        /* Close button styling */
+        .close-custom {
+            position: absolute;
+            right: 15px;
+            top: 15px;
+            border: none;
+            background: none;
+            font-size: 25px;
+            cursor: pointer;
+        }
+
+        /* Button styling */
+        .btn-submit {
+            background-color: #4e342e;
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            font-family: 'Arial', sans-serif;
+            font-size: 16px;
+            line-height: 1.5;
+            border-radius: 5px;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+        }
+
+        .btn-submit:hover {
+            background-color: #3e271e;
+        }
+
+        /* Input field styling */
+        .form-control {
+            margin-bottom: 10px; /* Adds spacing between form fields */
+            padding: 10px;
+            border: 1px solid #ced4da;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+<body>
+
+<a class="button" id="popupTrigger" href="#popupModal">Let me Pop up</a>
+
+<div id="popupModal" class="overlay">
+    <div class="modal-custom" role="dialog" aria-labelledby="modalTitle" aria-hidden="true">
+        <div class="modal-header">
+            <h5 class="modal-title" id="modalTitle">Please give us your information</h5>
+            <button type="button" class="close-custom" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <p>Receive notifications on New Collections and Sale Updates.</p>
+            <form id="newUserForm">
+                <div class="form-group">
+                    <input type="text" id="userName" class="form-control" placeholder="Full Name" name="fullname" required>
+                </div>
+                <div class="form-group">
+                    <input type="tel" id="userPhone" class="form-control" placeholder="Mobile" name="mobile">
+                </div>
+                <button type="submit" name="newUserSubmit" class="btn btn-submit">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
