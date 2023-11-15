7cd3f59ba6cee6ddd00d81833aea02c9ac89cfcb
adi2907 <adityaganguli@gmail.com>
2023-11-15 11:03:22 +0530

    fix

--- resources/views/theme_popups.blade.php
+++ resources/views/theme_popups.blade.php
@@ -1,94 +1,3 @@
-<!-- Bootstrap Modal 
-<style>
-    h1 {
-        text-align: center;
-        font-family: Tahoma, Arial, sans-serif;
-        color: #06D85F;
-        margin: 80px 0;
-    }
-    .box {
-        width: 40%;
-        margin: 0 auto;
-        background: rgba(255,255,255,0.2);
-        padding: 35px;
-        border: 2px solid #fff;
-        border-radius: 20px/50px;
-        background-clip: padding-box;
-        text-align: center;
-    }
-    
-    .button {
-        font-size: 1em;
-        padding: 10px;
-        color: #fff;
-        border: 2px solid #06D85F;
-        border-radius: 20px/50px;
-        text-decoration: none;
-        cursor: pointer;
-        transition: all 0.3s ease-out;
-    }
-    .button:hover {
-        background: #06D85F;
-    }
-    .overlay {
-        position: fixed;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        background: rgba(0, 0, 0, 0.7);
-        transition: opacity 500ms;
-        visibility: hidden;
-        opacity: 0;
-    }
-    .overlay:target {
-        visibility: visible;
-        opacity: 1;
-    }
-    
-    .popup {
-        margin: 70px auto;
-        padding: 20px;
-        background: #fff;
-        border-radius: 5px;
-        width: 30%;
-        position: relative;
-        transition: all 5s ease-in-out;
-    }
-    
-    .popup h2 {
-        margin-top: 0;
-        color: #333;
-        font-family: Tahoma, Arial, sans-serif;
-    }
-    .popup .close {
-        position: absolute;
-        top: 20px;
-        right: 30px;
-        transition: all 200ms;
-        font-size: 30px;
-        font-weight: bold;
-        text-decoration: none;
-        color: #333;
-    }
-    .popup .close:hover {
-        color: #06D85F;
-    }
-    .popup .content {
-        max-height: 30%;
-        overflow: auto;
-    }
-    
-    @media screen and (max-width: 700px){
-        .box{
-        width: 70%;
-        }
-        .popup{
-        width: 70%;
-        }
-    }
-</style>
-
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <style>
@@ -137,39 +46,41 @@
   }
 </style>
 
-<a class="button" id="popupModal" style="visibility:hidden" href="#popup1">Let me Pop up</a>
-<div style="font-family: Arial, sans-serif; height: 100vh;" id="popup1" class="overlay">
-    <div class="modal" id="popup1" tabindex="-1" aria-labelledby="desiModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-            <div class="modal-header">
-                <div class="header-box mx-auto text-center">
-                    <img src="images/brand_logo.png" alt="Brand Logo" class="brand-logo"> 
-                    <h5 class="modal-title">Become an Insider</h5>
+<body>
+    <a class="button" id="popupModal" style="visibility:hidden" href="#popup1">Let me Pop up</a>
+    <div style="font-family: Arial, sans-serif; height: 100vh;" id="popup1" class="overlay">
+        <div class="modal" id="popup1" tabindex="-1" aria-labelledby="desiModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <div class="header-box mx-auto text-center">
+                        <img src="images/brand_logo.png" alt="Brand Logo" class="brand-logo"> 
+                        <h5 class="modal-title">Become an Insider</h5>
+                    </div>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body text-center">
-                <p class="modal-description">Receive Whatsapp notifications on New Collections and Sale Updates.</p>
-                <form id="newUserForm">
-                <div class="form-group">
-                    <input type="text" id="userName" class="form-control" placeholder="Full Name" name="fullname">
+                <div class="modal-body text-center">
+                    <p class="modal-description">Receive Whatsapp notifications on New Collections and Sale Updates.</p>
+                    <form id="newUserForm">
+                    <div class="form-group">
+                        <input type="text" id="userName" class="form-control" placeholder="Full Name" name="fullname">
+                    </div>
+                    <div class="form-group">
+                        <input type="tel" id="userPhone" class="form-control" placeholder="Mobile" name="mobile">
+                    </div>
+                    </form>
                 </div>
-                <div class="form-group">
-                    <input type="tel" id="userPhone" class="form-control" placeholder="Mobile" name="mobile">
+                <div class="modal-footer justify-content-center">
+                    <button id="submit" type="submit" name="newUserSubmit" class="btn btn-submit">Submit</button>
                 </div>
-                </form>
-            </div>
-            <div class="modal-footer justify-content-center">
-                <button id="submit" type="submit" name="newUserSubmit" class="btn btn-submit">Submit</button>
-            </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
- Bootstrap Modal
+</body>
+ <!-- Bootstrap Modal
 <a class="button" id="popupModal" style="visibility:hidden" href="#popup1">Let me Pop up</a>
 <div style="font-family: Arial, sans-serif; height: 100vh;" id="popup1" class="overlay">
     <div class="popup">
@@ -189,88 +100,5 @@
             </form>
         </div>
     </div>
-</div>
--->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Popup Modal</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        /* Rest of your CSS */
-        .overlay {
-            display: none; /* Start hidden */
-            position: fixed;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            background-color: rgba(0, 0, 0, 0.5);
-            justify-content: center;
-            align-items: center;
-            z-index: 1050;
-        }
+</div> -->
 
-        .modal-custom {
-            background: white;
-            border-radius: 5px;
-            width: 500px; /* or your preferred size */
-            /* Rest of your modal styling */
-        }
-
-        /* Ensure button is visible for demonstration */
-        #popupTrigger {
-            visibility: visible;
-        }
-    </style>
-</head>
-<body>
-
-<!-- Button to trigger modal -->
-<a class="btn btn-primary" id="popupTrigger" href="#popupModal">Let me Pop up</a>
-
-<!-- Modal -->
-<div id="popupModal" class="overlay">
-    <div class="modal-custom" role="dialog">
-        <!-- Modal content -->
-        <div class="modal-header">
-            <h5 class="modal-title">Please give us your information</h5>
-            <button type="button" class="close">&times;</button>
-        </div>
-        <div class="modal-body">
-            <!-- Form -->
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-submit">Submit</button>
-        </div>
-    </div>
-</div>
-
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
-<script>
-    $(document).ready(function() {
-        // Trigger button shows the modal
-        $('#popupTrigger').click(function(e) {
-            e.preventDefault(); // Prevent default anchor behavior
-            $('#popupModal').show(); // Show the modal
-        });
-
-        // Clicking on the 'close' button hides the modal
-        $('.close').click(function() {
-            $('#popupModal').hide(); // Hide the modal
-        });
-
-        // Clicking anywhere outside of the modal will close it
-        $(window).click(function(e) {
-            if ($(e.target).is('#popupModal')) {
-                $('#popupModal').hide();
-            }
-        });
-    });
-</script>
-
-</body>
-</html>
