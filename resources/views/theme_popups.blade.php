3f7efab54041f99a20e46d87b2490ca693c24b3c
adi2907 <adityaganguli@gmail.com>
2023-11-16 15:54:38 +0530

    fix

--- resources/views/theme_popups.blade.php
+++ resources/views/theme_popups.blade.php
@@ -87,7 +87,7 @@
         }
     }
 </style>
-<a class="button" id="popupModal" style="visibility:hidden" href="#popup1">Let me Pop up</a>
+<!-- <a class="button" id="popupModal" style="visibility:hidden" href="#popup1">Let me Pop up</a>
 <div style="font-family: Arial, sans-serif; height: 100vh;" id="popup1" class="overlay">
     <div class="popup">
         <h2>Please give us your information</h2>
@@ -104,6 +104,37 @@
                 </div>
                 <button id="submit" type="submit" name="newUserSubmit" class="add-cart-cta">Submit</button>
             </form>
+        </div>
+    </div>
+</div> -->
+
+<a class="button" id="popupModal" style="visibility:hidden" href="#popup1">Let me Pop up</a>
+<div style="font-family: Arial, sans-serif; height: 100vh;" id="popup1" class="overlay">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class="header-box mx-auto text-center">
+                    <img src="images/brand_logo.png" alt="Brand Logo" class="brand-logo">
+                    <h5 class="modal-title">Become an Insider</h5>
+                </div>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        <div class="modal-body text-center">
+            <p class="modal-description">Receive Whatsapp notifications on New Collections and Sale Updates.</p>
+            <form>
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Full Name" name="fullname">
+            </div>
+            <div class="form-group">
+                <input type="tel" class="form-control" placeholder="Mobile" name="mobile">
+            </div>
+            </form>
+        </div>
+        <div class="modal-footer justify-content-center">
+            <button type="button" class="btn btn-submit">Submit</button>
+        </div>
         </div>
     </div>
 </div>
