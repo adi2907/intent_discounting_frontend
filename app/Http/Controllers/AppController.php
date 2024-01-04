204e2a5adb8397922b70e1a6164a88ed4941ac7a
Sid <sid.aims93@gmail.com>
2024-01-04 12:44:29 +0530

    Fix

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -22,21 +22,90 @@ class AppController extends Controller {
         
     }
 
-    public function checkAlmeScripts() {
+    public function checkAlmeScripts(Request $request) {
         $returnVal = [];
-        $shops = Shop::get();
+        $shops = Shop::where('shop_url', $request->shop)->get();
         foreach($shops as $shop) {
             if($this->verifyInstallation($shop)) {
                 $liveTheme = $this->getLiveThemeForShop($shop);
                 $returnVal['theme'][$shop->shop_url] = $liveTheme;
+                $assets = $this->getAssets($shop, $liveTheme);
                 $shopDetails = $this->getShopDetails($shop);
                 $returnVal['shop'][$shop->shop_url] = $shopDetails;
             } else {
                 $returnVal['result'][$shop->shop_url] = 'Invalid installation';
             }
         }
-
         return response()->json($returnVal);
+    }
+
+    private function getAssets($shop, $liveTheme) {
+        $appBlockTemplates = ['product', 'collection', 'index'];
+        $endpoint = getShopifyAPIURLForStore('themes/'.$liveTheme['id'].'/assets.json', $shop);
+        $headers = getShopifyAPIHeadersForStore($shop);
+        $response = $this->makeAnAPICallToShopify('GET', $endpoint,$headers);
+        return $response;
+
+        /**
+         * // Use `client.get` to request a list of themes on the shop
+        const {body:{themes}} = await client.get({
+        path: 'themes',
+        });
+        // Find the published theme
+        const publishedTheme = themes.find((theme) => theme.role === 'main');
+        // Retrieve a list of assets in the published theme
+        const {body:{assets}} = await client.get({
+        path: `themes/${publishedTheme.id}/assets`
+        });
+        // Check if JSON template files exist for the template specified in APP_BLOCK_TEMPLATES
+        const templateJSONFiles = assets.filter((file) => {
+        return APP_BLOCK_TEMPLATES.some(template => file.key === `templates/${template}.json`);
+        })
+        if (templateJSONFiles.length > 0 && (templateJSONFiles.length === APP_BLOCK_TEMPLATES.length)) {
+        console.log('All desired templates support sections everywhere!')
+        } else if (templateJSONFiles.length) {
+        console.log('Only some of the desired templates support sections everywhere.')
+        }
+        // Retrieve the body of JSON templates and find what section is set as `main`
+        const templateMainSections = (await Promise.all(templateJSONFiles.map(async (file, index) => {
+        const {body:{asset}} = await client.get({
+            path: `themes/${publishedTheme.id}/assets`,
+            query: { "asset[key]": file.key }
+        })
+
+        const json = JSON.parse(asset.value)
+        const main = Object.entries(json.sections).find(([id, section]) => id === 'main' || section.type.startsWith("main-"))
+        if (main) {
+            return assets.find((file) => file.key === `sections/${main[1].type}.liquid`);
+        }
+        }))).filter((value) => value)
+
+        // Request the content of each section and check if it has a schema that contains a
+        // block of type '@app'
+        const sectionsWithAppBlock = (await Promise.all(templateMainSections.map(async (file, index) => {
+        let acceptsAppBlock = false;
+        const {body:{asset}} = await client.get({
+            path: `themes/${publishedTheme.id}/assets`,
+            query: { "asset[key]": file.key }
+        })
+
+        const match = asset.value.match(/\{\%\s+schema\s+\%\}([\s\S]*?)\{\%\s+endschema\s+\%\}/m)
+        const schema = JSON.parse(match[1]);
+
+        if (schema && schema.blocks) {
+            acceptsAppBlock = schema.blocks.some((b => b.type === '@app'));
+        }
+
+        return acceptsAppBlock ? file : null
+        }))).filter((value) => value)
+        if (templateJSONFiles.length > 0 && (templateJSONFiles.length === sectionsWithAppBlock.length)) {
+            console.log('All desired templates have main sections that support app blocks!');
+        } else if (sectionsWithAppBlock.length) {
+            console.log('Only some of the desired templates support app blocks.');
+        } else {
+            console.log("None of the desired templates support app blocks");
+        }
+         */
     }
 
     /*
