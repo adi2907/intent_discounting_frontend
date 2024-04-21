0b97223be3c670c386baca6482b6d0d2ef43afe4
Chinmay Bhat <0310chinmay@gmail.com>
2024-04-21 05:20:23 -0700

    Added segments view for mobile

--- resources/views/layouts/mobile_sidebar.blade.php
+++ resources/views/layouts/mobile_sidebar.blade.php
@@ -2,6 +2,7 @@
 @php 
 $route = Route::currentRouteName()
 @endphp
+<link href="{{asset('css/sidebar.css')}}" type="text/css" rel="stylesheet" />
 <nav class="navbar navbar-expand-md navbar-light bg-light d-md-none w-100">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -12,6 +13,21 @@ $route = Route::currentRouteName()
             <li class="nav-item"><a href="{{route('productRacks')}}" class="nav-link">Product Collection</a></li>
             <li class="nav-item"><a href="{{route('notifications')}}" class="nav-link">Notifications</a></li>
             <li class="nav-item"><a href="{{route('identifiedUsers')}}" class="nav-link">Identified Users</a></li>
+            <li class="nav-item">
+    <div class="dropdown">
+        <a class="nav-link dropdown-toggle" href="{{ route('show.identifiedUsers') }}"  role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="mob-seg"></i> Segments
+        </a>
+
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <a class="mob-nav" href="{{ route('list.identified.user.segments') }}" @if($route == 'identifiedUsers') style="background-color: white; color:#1B4332;" @endif>
+                Identified Users Segment
+            </a>
+            
+            <!-- Add other dropdown items here -->
+        </div>
+    </div>
+</li>
         </ul>
     </div>
 </nav>
