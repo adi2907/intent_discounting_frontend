13324b4af7cc29bdddf9d144e85f33fe3b2ba327
Chinmay Bhat <0310chinmay@gmail.com>
2024-04-21 04:32:24 -0700

    fixed sidebar alignment

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -10,29 +10,29 @@ $route = Route::currentRouteName()
     </div>
     <ul class="nav flex-column">
         <li class="nav-item">
-            <a href="{{route('dashboard')}}" class="nav-link" @if($route == 'dashboard') style="background-color: white; color:#1B4332" @endif>
+            <a href="{{route('dashboard')}}" class="dash nav-link" @if($route == 'dashboard') style="background-color: white; color:#1B4332" @endif>
                 <i class="fas fa-tachometer-alt"></i> Dashboard
             </a>
         </li>
         <li class="nav-item">
             <a href="{{route('productRacks')}}" class="nav-link" @if($route == 'productRacks') style="background-color: white; color:#1B4332" @endif>
-                <i class="fas fa-shopping-bag"></i> Product Collection
+                <i class="fas fa-shopping-bag ml-2"></i> Product Collection
             </a>
         </li>
         <li class="nav-item">
             <a href="{{route('notifications')}}" class="nav-link" @if($route == 'notifications') style="background-color: white; color:#1B4332" @endif>
-                <i class="fas fa-message"></i> Notifications
+                <i class="fas fa-message ml-1"></i> Notifications
             </a>
         </li>
         <li class="nav-item">
             <a href="{{route('show.identifiedUsers')}}" class="nav-link" @if($route == 'identifiedUsers') style="background-color: white; color:#1B4332;" @endif>
-                <i class="fas fa-user"></i> Identified Users
+                <i class="fas fa-user ml-2"></i> Identified Users
             </a>
         </li>
         <li class="nav-item">
     <div class="dropdown">
         <a class="nav-link dropdown-toggle" href="{{ route('show.identifiedUsers') }}"  role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-th-list"></i> Segments
+            <i class="fas fa-th-list ml-1"></i> Segments
         </a>
 
         <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
