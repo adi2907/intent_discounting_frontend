16d5527f418d2416cad44622ce5d72db7bf5e540
Sid <sid.aims93@gmail.com>
2024-06-12 10:43:19 +0530

    blockRequestsUntil updated to 5 days instead of 30 seconds too

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -256,7 +256,7 @@ class AppController extends Controller {
         try {
             if($request->has('app_name') && $request->filled('app_name')) {
                 $shop = Shop::with('notificationSettings')->where('shop_url', $request->app_name)->first();
-                $blockRequestsUntil = strtotime('+30 seconds');
+                $blockRequestsUntil = strtotime('+5 days');
                 if($shop !== null && $shop->count() > 0) {
                     if(isset($shop->notificationSettings) && isset($shop->notificationSettings->sale_status)) {
                         if($shop->notificationSettings->sale_status == 1 || $shop->notificationSettings->sale_status === true) {
