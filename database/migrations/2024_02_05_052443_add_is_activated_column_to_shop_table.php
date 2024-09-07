e50940106a7c51fde952a2b51b475c555db77483
Sid <sid.aims93@gmail.com>
2024-09-08 00:51:45 +0530

    revert

--- database/migrations/2024_02_05_052443_add_is_activated_column_to_shop_table.php
+++ database/migrations/2024_02_05_052443_add_is_activated_column_to_shop_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('shops', function (Blueprint $table) {
+        Schema::table('shop', function (Blueprint $table) {
             $table->tinyInteger('isActivated')->default(false)->after('hmac');
         });
     }
